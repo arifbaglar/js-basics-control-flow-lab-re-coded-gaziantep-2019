b32b20cf071b5981009c29ef88c0132b9b1f7924
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 18:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(ride){
   let result
   if (ride <=400){result='This one is on me!'}
   else if (ride>2000) {result='I will gladly take your thirty bucks.';}
-  else if (ride>2500){result='does not allow rides over 2500 feet';}
+  else if (ride<2500){result='does not allow rides over 2500 feet';}
   return result;
 }
 
