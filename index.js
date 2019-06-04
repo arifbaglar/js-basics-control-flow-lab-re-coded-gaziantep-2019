501bb6cc8ee24ab16c72291a16b40a410db40b5f
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 17:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(199){
+function scuberGreetingForFeet(a=199){
   if (a <=400){'This one is on me!'}
   else if (a>2000) {'I will gladly take your thirty bucks.';}
   else if (a>2500){'No can do.';}
