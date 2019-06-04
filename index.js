85acb4378a7281ebfe33b35ff0abca553226dcae
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 18:33:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function scuberGreetingForFeet(ride){
 }
 
 function ternaryCheckCity(city){
-  var result = (city=="NYC") ? "Ok, sounds good.":"Old enough";
+  var result = (city=="NYC") ? 'Ok, sounds good.':'No go.';
 }
 
 function switchOnCharmFromTip(){
