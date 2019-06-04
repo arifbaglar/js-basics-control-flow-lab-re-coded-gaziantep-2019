bc09a0ad94c3b7bdbeda19616da16bc5addb7514
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 17:49:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function scuberGreetingForFeet(ride){
-  if (ride <=400){'This one is on me!'}
-  else if (ride>2000) {'I will gladly take your thirty bucks.';}
-  else if (ride>2500){'No can do.';}
-  
+  let result
+  if (ride <=400){result='This one is on me!'}
+  else if (ride>2000) {result='I will gladly take your thirty bucks.';}
+  else if (ride>2500){result='No can do.';}
+  return result;
 }
-function addFive(someNumber) {
-  
-  let result;
-  if (someNumber > 0) {
-    result = someNumber + 5;
-  }
- return result;
-}
+
 function ternaryCheckCity(){
   // Write your code here!
 }
