99348f6f32e4365a47ebbadb8303288590128f02
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 18:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(ride){
   let result
   if (ride <=400){result='This one is on me!'}
-  else if (ride>2000) {result='I will gladly take your thirty bucks.';}
+  else if (ride<2500) {result='I will gladly take your thirty bucks.';}
   else if (ride>2500){result='No can do.';}
   return result;
 }
